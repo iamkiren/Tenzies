0d23145c79d934c615c6c3a04df05f55d20ee34f
Kiren Raj <iamkiren@gmail.com>
2024-12-12 17:52:33 +0530

    added Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import Die from "./Die";
 
-function App() {
-  return <main></main>;
+export default function App() {
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 }
-
-export default App;
