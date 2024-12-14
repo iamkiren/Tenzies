7b90e0dce5ed7f10d57438130cc58cd4b6c8b77b
Kiren Raj <iamkiren@gmail.com>
2024-12-14 11:52:23 +0530

    implemented the dice color while its holded

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,3 +1,7 @@
-export default function Die({ value }) {
-  return <button>{value}</button>;
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+
+  return <button style={styles}>{props.value}</button>;
 }
